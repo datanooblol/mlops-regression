dff4f0e3880b86d36112f9cab7a94d8b72932e7d
bank <data.noob.lol@gmail.com>
2022-07-15 19:17:08 +0700

    first commit

---
+++ datanoob/datanooblol/hello_noob.py
@@ -0,0 +1,31 @@
+def yo_noob(name: str, num: int) -> str:
+    """
+    Greeting to all noobs
+    
+    :param str name: user name
+    :param int num: number
+    :returns: hello noob with name
+    :rtype: str
+    """
+    return f"yo noob: {name}{number}"
+
+def f_noob(name: str, num: int) -> str:
+    """
+    Greeting to all noobs
+    
+    :param str name: user name
+    :param int num: number
+    :returns: hello noob with name
+    :rtype: str
+    """
+    return f"f* noob: {name}{number}"
+
+def who_noob(name:str) -> str:
+    """
+    Find out who's the noob
+
+    :param str name: user name
+    :returns: name of the noob
+    :rtype: str
+    """
+    return f"Who's noob? {name}, you are a NOOB!!"
