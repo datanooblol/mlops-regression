dcd6abdd05f63aced3b3d9137872229a1c8fe5a5
bank <data.noob.lol@gmail.com>
2022-07-15 21:09:23 +0700

    update doc func and copy from _docs to document

--- datanoob/datanooblol/.ipynb_checkpoints/get_document-checkpoint.py
+++ datanoob/datanooblol/.ipynb_checkpoints/get_document-checkpoint.py
@@ -8,7 +8,7 @@ def init_doc():
     path = "/usr/src/datanoob/_docs"
     command = f"cd {path} && make html"
     os.system(command)
-    return
+    copy_to()
 
 def update_doc():
     """
@@ -19,4 +19,13 @@ def update_doc():
     path = "/usr/src/datanoob/_docs"
     command = f"cd {path} && make clean html"
     os.system(command)
-    return
+    copy_to()
+
+def copy_to():
+    """
+    Copy to document
+    
+    """
+    source = "/usr/src/datanoob/_docs/_build/html/*"
+    target = "/usr/src/datanoob/document"
+    os.system(f"cp -Rf {source} {target}")
